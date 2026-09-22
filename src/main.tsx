ac0cd6b62022d9b0b214e68e6ae0968d8155cfcd
turtle <78549147+CauchyRiemannEquations@users.noreply.github.com>
2026-09-22 17:42:19 +0900

    Remove legacy candy theme from new MVP

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./style.css";
-import "./candy-theme.css";
+
 createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode><App /></React.StrictMode>,
 );
